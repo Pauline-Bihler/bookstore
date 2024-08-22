7895c374a9da6a0976f180035694af1a57776c1e
Pauline Bihler <pauline.erika5@gmail.com>
2024-08-22 23:14:48 +0200

    Add scss

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'About',
+  description: 'Your classic weeb store',
+};
+
 export default function AboutPage() {
   return <div>This is my about page.</div>;
 }
--- app/books/page.js
+++ app/books/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Books',
+  description: 'Your classic weeb store',
+};
+
 export default function BooksPage() {
   return <div>Selection of books available.</div>;
 }

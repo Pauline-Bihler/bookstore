8a0b2ad4796691e0271b36c6fdf0dc31477fe2d5
Pauline Bihler <pauline.erika5@gmail.com>
2024-08-22 21:33:47 +0200

    Add nav pages

---
+++ app/books/page.js
@@ -0,0 +1,3 @@
+export default function BooksPage() {
+  return <div>Selection of books available.</div>;
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  );
 }
